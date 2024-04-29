d047edbea29f6bf5f29568a046fe0ff664ae2b6b
HyekJung <155180714+HyekJung@users.noreply.github.com>
2024-04-30 01:46:03 +0900

    Delete springboot-developer/springboot-developer directory

--- springboot-developer/springboot-developer/src/main/java/me/hyekjung/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package me.hyekjung.springbootdeveloper.repository;
-
-import me.hyekjung.springbootdeveloper.domain.Article;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
